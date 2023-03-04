7b0a2868ac17592da6078423a0b5def69a8b69d1
scratchyone <root@rachel.systems>
2023-03-04 11:45:17 -0500

    Update services

--- src/App.svelte
+++ src/App.svelte
@@ -1,126 +1,145 @@
 <script>
-	export let services;
+  export let services;
 </script>
 
-<style>
-	main {
-		margin: 0;
-		padding: 20px;
-		font-family: Open Sans;
-		color: #333333;
-	}
-	#header {
-		font-weight: 800;
-		font-size: 65px;
-		margin: 0;
-		margin-bottom: 20px;
-	}
-	@media (max-width: 1000px) {
-		#header {
-			font-size: 40px;
-		}
-	}
-
-	#holder {
-		display: grid;
-		grid-template-columns: repeat(2, 1fr);
-		gap: 20px;
-	}
-	@media (max-width: 1000px) {
-		#holder {
-			display: flex;
-			flex-direction: column;
-			gap: 20px;
-			flex-wrap: wrap;
-		}
-	}
-	.card_image > img {
-		width: 80px;
-		padding: 30px;
-	}
-	.card_image {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
-	.card {
-		display: flex;
-		flex-direction: row;
-		background: #f9f9f9;
-		box-shadow: 12.7879px 15.3775px 80px rgba(0, 0, 0, 0.07),
-			8.28847px 9.96693px 46.8519px rgba(0, 0, 0, 0.0531481),
-			4.92572px 5.9232px 25.4815px rgba(0, 0, 0, 0.0425185),
-			2.55759px 3.07551px 13px rgba(0, 0, 0, 0.035),
-			1.04198px 1.25299px 6.51852px rgba(0, 0, 0, 0.0274815),
-			0.236813px 0.284769px 3.14815px rgba(0, 0, 0, 0.0168519);
-		border-radius: 16px;
-	}
-	.card_title {
-		font-weight: 800;
-		font-size: 55px;
-		margin: 0px;
-	}
-	@media (max-width: 1000px) {
-		.card_title {
-			font-size: 35px;
-		}
-	}
-	.card_text {
-		font-weight: bold;
-		font-size: 24px;
-		margin: 0;
-		margin-top: -5px;
-		color: #333333;
-		word-break: break-word;
-	}
-	@media (max-width: 1000px) {
-		.card_text {
-			font-size: 20px;
-		}
-	}
-	.card_body {
-		display: flex;
-		flex-direction: column;
-		justify-content: space-between;
-		padding-right: 20px;
-		word-break: break-word;
-	}
-	.card_ip {
-		padding-bottom: 10px;
-	}
-	.card_ip > a {
-		font-weight: bold;
-		font-size: 25px;
-		color: #0e9ac7;
-		text-decoration: none;
-	}
-	@media (max-width: 1000px) {
-		.card_ip > a {
-			font-size: 20px;
-		}
-	}
-</style>
-
 <main>
-	<h1 id="header">Home Network Guide</h1>
-	<div id="holder">
-		{#each services as service}
-			<div class="card">
-				<div class="card_image">
-					<img alt={service.name + ' Logo'} src={service.image} />
-				</div>
-				<div class="card_body">
-					<div>
-						<h1 class="card_title">{service.name}</h1>
-						<h3 class="card_text">{service.description}</h3>
-					</div>
-					<div class="card_ip">
-						<a
-							target="_blank"
-							href={'http://' + service.ip}>{service.ip}</a>
-					</div>
-				</div>
-			</div>
-		{/each}
-	</div>
+  <h1 id="header">Home Network Guide</h1>
+  <div id="holder">
+    {#each services as service}
+      <div class="card">
+        <div class="card_image">
+          <img alt={service.name + ' Logo'} src={service.image} />
+        </div>
+        <div class="card_body">
+          <div>
+            <h1 class="card_title">{service.name}</h1>
+            <h3 class="card_text">{service.description}</h3>
+          </div>
+          <div>
+            <div class="card_hostname">
+              <a
+                target="_blank"
+                href={'http://' + (service.hostname || service.ip)}
+                >{service.hostname || service.ip}</a
+              >
+            </div>
+            <div class="card_ip">
+              {#if service.hostname}
+                <a target="_blank" href={'http://' + service.ip}>{service.ip}</a
+                >
+              {/if}
+            </div>
+          </div>
+        </div>
+      </div>
+    {/each}
+  </div>
 </main>
+
+<style>
+  main {
+    margin: 0;
+    padding: 20px;
+    font-family: Open Sans;
+    color: #333333;
+  }
+  #header {
+    font-weight: 800;
+    font-size: 65px;
+    margin: 0;
+    margin-bottom: 20px;
+  }
+  @media (max-width: 1000px) {
+    #header {
+      font-size: 40px;
+    }
+  }
+
+  #holder {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 20px;
+  }
+  @media (max-width: 1000px) {
+    #holder {
+      display: flex;
+      flex-direction: column;
+      gap: 20px;
+      flex-wrap: wrap;
+    }
+  }
+  .card_image > img {
+    width: 80px;
+    padding: 30px;
+  }
+  .card_image {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .card {
+    display: flex;
+    flex-direction: row;
+    background: #f9f9f9;
+    box-shadow: 12.7879px 15.3775px 80px rgba(0, 0, 0, 0.07),
+      8.28847px 9.96693px 46.8519px rgba(0, 0, 0, 0.0531481),
+      4.92572px 5.9232px 25.4815px rgba(0, 0, 0, 0.0425185),
+      2.55759px 3.07551px 13px rgba(0, 0, 0, 0.035),
+      1.04198px 1.25299px 6.51852px rgba(0, 0, 0, 0.0274815),
+      0.236813px 0.284769px 3.14815px rgba(0, 0, 0, 0.0168519);
+    border-radius: 16px;
+  }
+  .card_title {
+    font-weight: 800;
+    font-size: 55px;
+    margin: 0px;
+  }
+  @media (max-width: 1000px) {
+    .card_title {
+      font-size: 35px;
+    }
+  }
+  .card_text {
+    font-weight: bold;
+    font-size: 24px;
+    margin: 0;
+    margin-top: -5px;
+    color: #333333;
+    word-break: break-word;
+  }
+  @media (max-width: 1000px) {
+    .card_text {
+      font-size: 20px;
+    }
+  }
+  .card_body {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding-right: 20px;
+    word-break: break-word;
+  }
+  .card_ip {
+    padding-bottom: 10px;
+  }
+  .card_ip > a {
+    padding-top: 0;
+    font-weight: 700;
+    font-size: 15px;
+    color: #a5a5a5;
+    text-decoration: none;
+  }
+  .card_hostname > a {
+    font-weight: bold;
+    font-size: 25px;
+    color: #0e9ac7;
+    text-decoration: none;
+  }
+  @media (max-width: 1000px) {
+    .card_hostname > a {
+      font-size: 20px;
+    }
+    .card_ip > a {
+    }
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,30 +1,111 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body,
-	props: {
-		services: [{
-			"name": "PiHole",
-			"image": "https://upload.wikimedia.org/wikipedia/en/thumb/1/15/Pi-hole_vector_logo.svg/1200px-Pi-hole_vector_logo.svg.png",
-			"description": "PiHole is a DNS ad blocker.",
-			"ip": "192.168.2.111"
-		}, {
-			"name": "VMware",
-			"image": "https://www.drupal.org/files/project-images/vmware.png",
-			"description": "VMware is a virtualization tool powering the downstairs server.",
-			"ip": "192.168.2.168"
-		}, {
-			"name": "Jellyfin",
-			"image": "https://forum.jellyfin.org/uploads/default/original/1X/bcc1ca50528695f31a888ad67607e016fb3050c1.png",
-			"description": "Jellyfin is a media server for movies and TV.",
-			"ip": "192.168.2.112"
-		}, {
-			"name": "Network Guide",
-			"image": "https://www.logolynx.com/images/logolynx/df/dfc95540c225d6841233d7f74c9a2a11.png",
-			"description": "A guide to all devices and services on our home network.",
-			"ip": "192.168.2.113"
-		}]
-	}
+  target: document.body,
+  props: {
+    services: [
+      {
+        name: 'PiHole',
+        image:
+          'https://upload.wikimedia.org/wikipedia/en/thumb/1/15/Pi-hole_vector_logo.svg/1200px-Pi-hole_vector_logo.svg.png',
+        description: 'PiHole is a DNS ad blocker.',
+        ip: '192.168.2.111',
+      },
+      {
+        name: 'VMware',
+        image: 'https://www.drupal.org/files/project-images/vmware.png',
+        description:
+          'VMware is a virtualization tool powering the downstairs server.',
+        ip: '192.168.2.168',
+        hostname: 'vmware.local',
+      },
+      {
+        name: 'Jellyfin',
+        image:
+          'https://features.jellyfin.org/images/logos/a7Lx9nYDzWuDR94Az8Yum7neWMvNMndkm9qr4QVtmjaMrOHDLisS5K7LJctTRzK9-icon-transparent.png?size=200',
+        description: 'Jellyfin is a media server for movies and TV.',
+        ip: '192.168.2.112:8096',
+      },
+      {
+        name: 'Plex',
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/f/f0/Plex_vector_logo.svg',
+        description: 'Plex is an alternative media server for movies and TV.',
+        ip: '192.168.2.112:32400',
+      },
+
+      {
+        name: 'Home Assistant',
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Home_Assistant_Logo.svg/1200px-Home_Assistant_Logo.svg.png',
+        description: 'Home Assistant is a home automation platform.',
+        ip: 'homeassistant.local:8123',
+      },
+      {
+        name: 'Sonarr',
+        image:
+          'https://developer.asustor.com/uploadIcons/0020_23588_1465489647_0020_999_1439891916_0020_17730_1400232290_nzbDrone_icon_256x256.png',
+        description: 'Sonarr is an automatic download tool for TV shows.',
+        ip: '192.168.2.112:8989',
+      },
+      {
+        name: 'Radarr',
+        image: 'https://cdn.rawgit.com/Radarr/Radarr/develop/Logo/256.png',
+        description: 'Radarr is an automatic download tool for movies.',
+        ip: '192.168.2.112:7878',
+      },
+      {
+        name: 'Prowlarr',
+        image:
+          'https://raw.githubusercontent.com/linuxserver/docker-templates/master/linuxserver.io/img/prowlarr-banner.png',
+        description: 'Prowlarr is an indexer for Radarr and Sonarr.',
+        ip: '192.168.2.112:9696',
+      },
+      {
+        name: 'qBittorrent',
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/New_qBittorrent_Logo.svg/1200px-New_qBittorrent_Logo.svg.png',
+        description: 'qBittorrent is a torrent download client.',
+        ip: '192.168.2.112:8080',
+      },
+      {
+        name: 'WireGuard',
+        image:
+          'https://camo.githubusercontent.com/33116b1a48fc77cac43a0464c33d6f427d0655c9d0e860b1e43e41adeaa83c7b/68747470733a2f2f692e696d6775722e636f6d2f704341476a69612e706e67',
+        description: 'WireGuard is a fast and efficient VPN server.',
+        ip: '192.168.2.114',
+        hostname: 'wireguard.local',
+      },
+      {
+        name: 'Tdarr',
+        image: 'https://tdarr.io/static/media/logo3.02a3f4a3.png',
+        description: 'Tdarr is a distributed media transcoding tool.',
+        ip: '192.168.2.112:8265',
+      },
+      {
+        name: 'Grafana',
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Grafana_logo.svg/640px-Grafana_logo.svg.png',
+        description: 'Grafana is a time-series dashboard.',
+        ip: '192.168.2.112:3000',
+      },
+      {
+        name: 'HandBrake',
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/d/d9/HandBrake_Icon.png',
+        description: 'HandBrake is a media transcoding tool.',
+        ip: '192.168.2.112:5800',
+      },
+      {
+        name: 'Network Guide',
+        image:
+          'https://www.logolynx.com/images/logolynx/df/dfc95540c225d6841233d7f74c9a2a11.png',
+        description: 'A guide to all devices and services on our home network.',
+        ip: '192.168.2.113',
+        hostname: 'localhomepage.local',
+      },
+    ],
+  },
 });
 
 export default app;
